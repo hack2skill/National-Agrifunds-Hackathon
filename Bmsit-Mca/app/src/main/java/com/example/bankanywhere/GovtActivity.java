cb05d8633a47e2ae800f1f858102d42a6aac40c0
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-08-26 22:08:42 +0530

    Government View updated. Added drop down menus of schemes. Added register button when register is clicked it asks for aadhar number authenticate button also added, but has no functionality as of now.

--- Bmsit-Mca/app/src/main/java/com/example/bankanywhere/GovtActivity.java
+++ Bmsit-Mca/app/src/main/java/com/example/bankanywhere/GovtActivity.java
@@ -5,32 +5,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GovtActivity extends AppCompatActivity {
-    EditText name,accno,passwd,phno,mail;
+    Spinner scheme_select;
+    Button register,auth;
+    TextView aadhar_title;
+    EditText aadhar_value;
+    String[] schemes={"Farmers Crop Insurance","Pradhan Mantri Samman nidhi yojana",
+    "Kisan Credit Card Scheme"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.government_layout);
+        register=(Button) findViewById(R.id.register);
+        auth=(Button) findViewById(R.id.auth);
+        scheme_select=(Spinner) findViewById(R.id.schemeSpinner);
+        ArrayAdapter<String>adapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,schemes);
+        scheme_select.setAdapter(adapter);
+        aadhar_title=(TextView) findViewById(R.id.aadhar_title);
+        aadhar_value=(EditText) findViewById(R.id.aadhar_value);
 
-        Button register= (Button) findViewById(R.id.signup);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name=(EditText) findViewById(R.id.name);
-                accno=(EditText) findViewById(R.id.accno);
-                passwd=(EditText) findViewById(R.id.passwd);
-                phno=(EditText) findViewById(R.id.phno);
-                mail=(EditText) findViewById(R.id.email);
-                validateRegister();
+                aadhar_title.setVisibility(1);
+                aadhar_value.setVisibility(1);
+                auth.setVisibility(1);
             }
         });
-    }
-    public void validateRegister(){
-        if(passwd.getText().toString().isEmpty()){
-            Toast.makeText(this,"Enter Password",Toast.LENGTH_LONG).show();
-        }
     }
 }
